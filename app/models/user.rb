8e6bbeaa28a1db4b4a14ddab3165f0c80f56e3a1
Valentin Syrovatskiy <vsyrovat@gmail.com>
2011-07-02 07:27:28 +1100

    replace user-2-country link type from has_and_belong_to_many to through model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	has_and_belongs_to_many :countries, :join_table => "user_2_country", :association_foreign_key => 'country_code'
+#	has_and_belongs_to_many :countries, :join_table => "user_2_country", :association_foreign_key => 'country_code'
+	has_many :visits
+	has_many :countries, :through => :visits
 
 	def currencies
 		self.countries.map{|country| country.currencies}.flatten
